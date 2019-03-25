683bfde446c85fc7cd5e9ffc184c7bac7187c68d
Matej Bransky <bransky@logio.cz>
2019-03-25 10:11:03 +0100

    init db with sequelize cli

---
+++ server/db/models/context.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Context = sequelize.define(
+    'Context',
+    {
+      name: DataTypes.STRING,
+    },
+    {}
+  );
+  Context.associate = function(/*models*/) {
+    // associations can be defined here
+  };
+  return Context;
+};
---
+++ server/db/models/task.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Task = sequelize.define(
+    'Task',
+    {
+      name: DataTypes.STRING,
+      done: DataTypes.BOOLEAN,
+      description: DataTypes.TEXT,
+    },
+    {}
+  );
+  Task.associate = function(/*models*/) {
+    // associations can be defined here
+  };
+  return Task;
+};
