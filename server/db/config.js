96a0c94bac9726d19a6363c005c906a2e0f6ad67
Matej Bransky <bransky@logio.cz>
2019-03-25 12:56:44 +0100

    support env variables for dev and prod in sequelize config

--- server/db/config.js
+++ server/db/config.js
@@ -1,10 +1,14 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(process.cwd(), '../.env') });
+
 module.exports = {
   development: {
-    username: 'root',
-    password: null,
-    database: 'database_development',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: process.env.DEV_DB_USER,
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.DEV_DB_NAME,
+    host: process.env.DEV_DB_HOST,
+    port: process.env.DEV_DB_PORT,
+    dialect: process.env.DEV_DB_DIALECT,
   },
   test: {
     username: process.env.TEST_DB_USER,
@@ -15,10 +19,11 @@ module.exports = {
     dialect: process.env.TEST_DB_DIALECT,
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: process.env.PROD_DB_USER,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOST,
+    port: process.env.PROD_DB_PORT,
+    dialect: process.env.PROD_DB_DIALECT,
   },
 };
